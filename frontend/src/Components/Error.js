18a219eb8f2238424a6c646931b4dbc8c5d0900a
shruti1234-5 <shrutisharma620425@gmail.com>
2025-07-09 12:14:15 +0530

    Update dependencies and refactor API endpoints for login and registration

--- frontend/src/Components/Error.js
+++ frontend/src/Components/Error.js
@@ -1,19 +1,3 @@
-// import React from 'react'
-
-// function Error({message}) {
-//   return (
-//     <div>
-//        <div className='alert alert-danger' role='alert'>
-//     {message}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Error;
-
-
-
 import React from 'react'
 
 function Error({message}) {
